492a5e0e756a5b896e433242b4420c8a6a35585c
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2018-06-19 15:33:41 +0530

    revise player model, add roto script

--- general/models.py
+++ general/models.py
@@ -4,12 +4,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Player(models.Model):
+    uid = models.CharField(max_length=100, null=True, blank=True)
     name = models.CharField(max_length=100)
     salary = models.IntegerField()
     position = models.CharField(max_length=10)
     points = models.FloatField()
     value = models.FloatField()
     team = models.CharField(max_length=50)
+    updated_at = models.DateField(auto_now=True)
+
+    class Meta:
+        unique_together = ('name', 'team')
 
     def __str__(self):
-    	return self.name
+        return self.name
