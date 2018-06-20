4194e6cb22ce9d1d73c9f60f0455d30688457438
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2018-06-21 01:51:05 +0530

    revise import fanduel logic

--- general/imports.py
+++ general/imports.py
@@ -5,6 +5,12 @@ from django.http import HttpResponse
 from django.conf import settings
 from general.models import *
 
+POSITION_DICT = {
+    'FWD': 'F',
+    'MID': 'M',
+    'DEF': 'D',
+    'GK': 'GK'
+}
 
 def import_player(request):
     path = settings.BASE_DIR + '/data/roto.csv'
@@ -33,15 +39,26 @@ def import_fanduel(path='media/root/Work/Steven/fantasy_soccer/data/FanDuel-FIFA
     with open(path) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            fanduel_list.append([row['Id'], row['Nickname'], row['First Name'], row['Last Name'], row['Team']])
+            player_ = Player(uid=row['Id'],
+                             name=row['Nickname'],
+                             salary=row['Salary'],
+                             position=POSITION_DICT[row['Position']],
+                             team=row['Team'],
+                             game_category=file_name)
+            fanduel_list.append([player_, row['First Name'], row['Last Name']])
 
     for ii in Player.objects.all():
         for fi in fanduel_list:
             name_list = ii.name.split(' ')
-            if ((ii.name.strip(' GTD') in fi[1]) or (name_list[0] in fi[2] and name_list[-1] in fi[3])) and (ii.team == fi[4]):
-                ii.uid = fi[0]
+            if ((ii.name.strip(' GTD') in fi[0].name) or (name_list[0] in fi[1] and name_list[-1] in fi[2])) and (ii.team == fi[0].team):
+                ii.uid = fi[0].uid
                 ii.game_category = ','.join(set((ii.game_category or '').split(',')+[file_name]))
                 ii.save()
+                fi[0].uid = ''
 
+    for fi in fanduel_list:
+        if fi[0].uid:
+            fi[0].save()
+            
     print Player.objects.all().exclude(uid__isnull=True).count()
     return file_name
